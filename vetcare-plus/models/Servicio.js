fd496d498fda459fda3487270dc47bd9bb61ec59
Jholman <camiflako17@hotmail.com>
2024-11-17 12:32:01 -0500

    Optimización en archivo.models-servicio.js

--- vetcare-plus/models/Servicio.js
+++ vetcare-plus/models/Servicio.js
@@ -1,9 +1,22 @@
 const mongoose = require('mongoose');
 
+// Definición del esquema de Servicio
 const servicioSchema = new mongoose.Schema({
-    tipo: { type: String, enum: ['Hospedaje', 'Vacunación', 'Baño'], required: true },
-    fecha: { type: Date, required: true },
-    mascota: { type: mongoose.Schema.Types.ObjectId, ref: 'Mascota', required: true }
+    tipo: {
+        type: String,
+        enum: ['Hospedaje', 'Vacunación', 'Baño'],
+        required: true
+    },
+    fecha: {
+        type: Date,
+        required: true
+    },
+    mascota: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Mascota',
+        required: true
+    }
 });
 
+// Creación y exportación del modelo Servicio
 module.exports = mongoose.model('Servicio', servicioSchema);
