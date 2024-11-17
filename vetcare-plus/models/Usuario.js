f890801da78313a684ab1bb96c1f46c9d28a2caf
Jholman <camiflako17@hotmail.com>
2024-11-17 12:31:01 -0500

    Optimización en archivo models- usuario.js

--- vetcare-plus/models/Usuario.js
+++ vetcare-plus/models/Usuario.js
@@ -1,11 +1,30 @@
 const mongoose = require('mongoose');
 
+// Definición del esquema de Usuario
 const usuarioSchema = new mongoose.Schema({
-    nombre: { type: String, required: true },
-    correo: { type: String, required: true, unique: true },
-    contraseña: { type: String, required: true },
-    tipoUsuario: { type: String, enum: ['Propietario', 'Administrador', 'Veterinario', 'Cuidador'], required: true },
-    mascotas: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Mascota' }]
+    nombre: {
+        type: String,
+        required: true
+    },
+    correo: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    contraseña: {
+        type: String,
+        required: true
+    },
+    tipoUsuario: {
+        type: String,
+        enum: ['Propietario', 'Administrador', 'Veterinario', 'Cuidador'],
+        required: true
+    },
+    mascotas: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Mascota'
+    }]
 });
 
+// Creación y exportación del modelo Usuario
 module.exports = mongoose.model('Usuario', usuarioSchema);
