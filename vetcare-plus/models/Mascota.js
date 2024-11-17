181759bd3dd0343b6f4993a7df72d020d85a5c9d
Jholman <camiflako17@hotmail.com>
2024-11-17 12:32:57 -0500

    optimización en archivo models.Mascota.js

--- vetcare-plus/models/Mascota.js
+++ vetcare-plus/models/Mascota.js
@@ -1,12 +1,30 @@
 const mongoose = require('mongoose');
 
+// Definición del esquema de Mascota
 const mascotaSchema = new mongoose.Schema({
-    nombre: { type: String, required: true },
-    raza: { type: String },
-    edad: { type: Number },
-    propietario: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true },
-    historialMedico: String,
-    servicios: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Servicio' }]
+    nombre: {
+        type: String,
+        required: true
+    },
+    raza: {
+        type: String
+    },
+    edad: {
+        type: Number
+    },
+    propietario: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Usuario',
+        required: true
+    },
+    historialMedico: {
+        type: String
+    },
+    servicios: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Servicio'
+    }]
 });
 
+// Creación y exportación del modelo Mascota
 module.exports = mongoose.model('Mascota', mascotaSchema);
